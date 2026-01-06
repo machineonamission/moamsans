235a5230f576fe59a34eabbcd00c373b5491e6ae
Melody <machineonamission@gmail.com>
2026-01-05 23:01:30 -0600

    it would be way smarter to just use fontforge apis directly instead of this stupid svg step

--- generate.py
+++ generate.py
@@ -21,14 +21,13 @@ class Letter:
     lines: list[Line]
 
 
-# todo distinguish derived and forced grid
 @dataclass
 class Grid:
     xmin: int
     xmax: int
     ymin: int
     ymax: int
-    em_height: int
+    em_height: float
     x_ratio: float
     stroke_ratio: float = None
     factory_stroke: float = None
@@ -70,6 +69,10 @@ class Grid:
             return self.factory_stroke
 
     @property
+    def hsw(self) -> float:
+        return self.stroke_width / 2
+
+    @property
     def ascent(self) -> float:
         return self.ymax * self.ystep + self.stroke_width
 
@@ -95,33 +98,48 @@ class Grid:
         return self.pixel_pos(point.x, point.y)
 
 
-letters = {
-    "h": Letter([
+lower_letters = {
+    "v": Letter([
         Line([
-            Point(0, 3),
-            Point(0, -1)
+            Point(0, 2),
+            Point(1, 0),
+            Point(2, 2),
         ]),
-        Line([
-            Point(0, 1),
-            Point(1, 1, True),
-            Point(1, 0)
-        ])
     ]),
 }
 
-grid = Grid(
+upper_letters = {
+}
+
+lower_grid = Grid(
     xmin=0,
     xmax=2,
     ymin=-1,
     ymax=3,
     em_height=1000,
     x_ratio=0.8,
-    stroke_ratio=1/2,
+    stroke_ratio=1 / 2,
 )
 
-print(grid.ascent, grid.descent)
+upper_grid = Grid(
+    xmin=0,
+    xmax=2,
+    ymin=0,
+    ymax=2,
+    em_height=lower_grid.ascent,
+    x_ratio=0.8,
+    factory_stroke=lower_grid.stroke_width,
+)
 
-def normal(p: tuple[int, int]) -> tuple[float, float]:
+master_list = [
+    (lower_grid, lower_letters),
+    (upper_grid, upper_letters),
+]
+
+print(lower_grid.ascent, lower_grid.descent)
+
+
+def normal(p: tuple[float, float]) -> tuple[float, float]:
     x, y = p
     length = (x ** 2 + y ** 2) ** 0.5
     if length == 0:
@@ -129,48 +147,68 @@ def normal(p: tuple[int, int]) -> tuple[float, float]:
     return x / length, y / length
 
 
-for letter_key, letter in letters.items():
-    letter: Letter
-    # grid.xmin * grid.xstep, grid.ymin * grid.ystep, grid.x_ratio * grid.em_height, grid.em_height
-    with cairo.SVGSurface(f"svgs/{letter_key}.svg", grid.width * grid.xstep + grid.stroke_width, grid.height * grid.ystep + grid.stroke_width) as surface:
-        context = cairo.Context(surface)
-        context.translate(0, grid.ystep * grid.ymax + grid.stroke_width)
-        context.scale(1, -1)
-        context.set_line_width(grid.stroke_width)
-        context.set_line_cap(cairo.LINE_CAP_SQUARE)
-        for line in letter.lines:
-            init = line.points[0]
-            context.move_to(
-                *grid.point_pos(init)
-            )
-            for i in range(1, len(line.points)):
-                point = line.points[i]
-                prev_point = line.points[i - 1]
-                next_point = line.points[i + 1] if i + 1 < len(line.points) else None
+for grid, letters in master_list:
+    grid: Grid
+    for letter_key, letter in letters.items():
+        letter: Letter
+        # grid.xmin * grid.xstep, grid.ymin * grid.ystep, grid.x_ratio * grid.em_height, grid.em_height
+        with cairo.SVGSurface(f"svgs/{letter_key}.svg", grid.width * grid.xstep + grid.stroke_width,
+                              grid.height * grid.ystep + grid.stroke_width) as surface:
+            context = cairo.Context(surface)
+            context.translate(0, grid.ystep * grid.ymax + grid.stroke_width)
+            context.scale(1, -1)
+            context.set_line_width(grid.stroke_width)
+            context.set_line_cap(cairo.LINE_CAP_SQUARE)
+            for line in letter.lines:
+                context.save()  # restore point for clip shit
 
-                x, y = grid.point_pos(point)
+                minx = min(grid.point_pos(p)[0] for p in line.points)
+                maxx = max(grid.point_pos(p)[0] for p in line.points)
+                miny = min(grid.point_pos(p)[1] for p in line.points)
+                maxy = max(grid.point_pos(p)[1] for p in line.points)
+                width = maxx - minx
+                height = maxy - miny
 
-                prev_x, prev_y = grid.point_pos(prev_point)
-                next_x, next_y = grid.point_pos(next_point) if next_point else (None, None)
-                if point.rounded:
-                    prev_diff = (x - prev_x, y - prev_y)
-                    next_diff = (x - next_x, y - next_y)
-                    prev_normal = normal(prev_diff)
-                    next_normal = normal(next_diff)
-                    offset_x = x - prev_normal[0] * (grid.stroke_width / 2) - next_normal[0] * (
-                            grid.stroke_width / 2)
-                    offset_y = y - prev_normal[1] * (grid.stroke_width / 2) - next_normal[1] * (
-                            grid.stroke_width / 2)
+                context.rectangle(minx - grid.hsw,
+                                  miny - grid.hsw,
+                                  width + grid.stroke_width,
+                                  height + grid.stroke_width)
+                context.clip()
 
-                    a1 = math.atan2(prev_diff[1], prev_diff[0])
-                    # a1 = min(math.tau - a1, a1)
-                    a2 = math.atan2(next_diff[1], next_diff[0])
-                    # a2 = min(math.tau - a2, a2)
+                init = line.points[0]
+                context.move_to(
+                    *grid.point_pos(init)
+                )
+                for i in range(1, len(line.points)):
+                    point = line.points[i]
+                    prev_point = line.points[i - 1]
+                    next_point = line.points[i + 1] if i + 1 < len(line.points) else None
 
-                    if a1 < a2:
-                        context.arc_negative(offset_x, offset_y, grid.stroke_width / 2, a2, a1)
+                    x, y = grid.point_pos(point)
+
+                    prev_x, prev_y = grid.point_pos(prev_point)
+                    next_x, next_y = grid.point_pos(next_point) if next_point else (None, None)
+                    if point.rounded:
+                        prev_diff = (x - prev_x, y - prev_y)
+                        next_diff = (x - next_x, y - next_y)
+                        prev_normal = normal(prev_diff)
+                        next_normal = normal(next_diff)
+                        offset_x = x - prev_normal[0] * grid.hsw - next_normal[0] * (
+                            grid.hsw)
+                        offset_y = y - prev_normal[1] * grid.hsw - next_normal[1] * (
+                            grid.hsw)
+
+                        a1 = math.atan2(prev_diff[1], prev_diff[0])
+                        # a1 = min(math.tau - a1, a1)
+                        a2 = math.atan2(next_diff[1], next_diff[0])
+                        # a2 = min(math.tau - a2, a2)
+
+                        if a1 < a2:
+                            context.arc_negative(offset_x, offset_y, grid.hsw, a2, a1)
+                        else:
+                            context.arc(offset_x, offset_y, grid.hsw, a2, a1)
                     else:
-                        context.arc(offset_x, offset_y, grid.stroke_width / 2, a2, a1)
-                else:
-                    context.line_to(x, y)
-            context.stroke()
+                        context.line_to(x, y)
+                context.stroke()
+                # essentially leave the clipping mask?
+                context.restore()
